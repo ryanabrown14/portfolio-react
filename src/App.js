3a574471fc06053253afe95092a13834ea7c20e6
ry brown <ryan.alex.brown@gmail.com>
2020-12-19 20:40:18 -0700

    basic files set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio;"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header>
+
+      </Header>
+      <main>
+        <div>
+
+        </div>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
